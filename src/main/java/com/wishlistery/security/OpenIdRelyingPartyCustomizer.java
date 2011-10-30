fb56980d62415885aa3dc889dd9a7b7432c72aa9
jeff_skj <jeff_skj@f97d9543-abb0-7445-985f-e055af8b4737>
2011-10-30 23:02:49 +0000

    added open ID customizer properties
    git-svn-id: http://mongoose:8090/svn/wishlist/trunk@12 f97d9543-abb0-7445-985f-e055af8b4737

--- src/main/java/com/wishlistery/security/OpenIdRelyingPartyCustomizer.java
+++ src/main/java/com/wishlistery/security/OpenIdRelyingPartyCustomizer.java
@@ -1,16 +1,24 @@
 package com.wishlistery.security;
 
+import java.util.Properties;
+
 import javax.enterprise.event.Observes;
+import javax.inject.Inject;
 import javax.servlet.ServletContext;
 
 import org.jboss.seam.security.external.openid.api.OpenIdRelyingPartyConfigurationApi;
+import org.jboss.solder.resourceLoader.Resource;
 import org.jboss.solder.servlet.event.Initialized;
 
 
 public class OpenIdRelyingPartyCustomizer {
+    @Inject
+    @Resource("open-id.properties")
+    private Properties properties;
+    
     public void servletInitialized(@Observes @Initialized final ServletContext context, OpenIdRelyingPartyConfigurationApi op) {
-        op.setHostName("localhost");
-        op.setPort(8080);
+        op.setHostName(properties.getProperty("host", "localhost"));
+        op.setPort(Integer.parseInt(properties.getProperty("port", "8080")));
         op.setProtocol("http");
     }
 }
